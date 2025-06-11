14787e4d7a43a628c35378c269bed375bf02fd32
Tatiana Garcia <tatiana.garcia.vald@gmail.com>
2025-06-11 09:05:15 +0200

    Delete all coments from tictactoe.js and add an Image

--- tictactoe.js
+++ tictactoe.js
@@ -1,37 +1,24 @@
 const game = {
-    // booleà per controlar el canvi de torns
     xTurn: true,
-    // estat de X, matriu de strings
     xState: [],
-    // estat de O, matriu de strings
     oState: [],
-    // possibles combinacions que guanyen la partida
     winningStates: [
-        // Files
         ['0', '1', '2'],
         ['3', '4', '5'],
         ['6', '7', '8'],
-
-        // Columnes
         ['0', '3', '6'],
         ['1', '4', '7'],
         ['2', '5', '8'],
-
-        // Diagonal
         ['0', '4', '8'],
         ['2', '4', '6']
     ]
 };
 
-// El teu codi aquí
-
-// cargar marcador desde el localStorage o iniciar desde cero
 let scores = JSON.parse(localStorage.getItem("scores")) || {
     deadpool: { wins: 0, losses: 0, total: 0 },
     wolverine: { wins: 0, losses: 0, total: 0 }
 };
 
-//Mostrar marcador
 function updateScoreboard() {
     document.getElementById("deadpool-wins").textContent = scores.deadpool.wins;
     document.getElementById("deadpool-losses").textContent = scores.deadpool.losses;
@@ -53,16 +40,12 @@ restartContainer.classList.remove("hidden");
 
 board.classList.add("hidden");
 
-//creo el tablero
-
 for (let i = 0; i <= 8; i++) {
     let divCell = document.createElement("div");
     divCell.classList.add("cell");
     divCell.id = i;
     board.appendChild(divCell);
 }
-
-// muesto de quién es el turno
 
 let turnText = document.querySelector(".container-text");
 let turnSpan = document.querySelector(".text-turn");
@@ -78,26 +61,20 @@ function showTurn() {
 }
 
 board.addEventListener('click', event => {
-    // Accedeix als elements html necessaris
     const cell = event.target;
 
-    // El jugador fa click a una casella buida
     if (!cell.classList.contains("cell")) {
         alert("click on the board!!");
         return;
     }
-
-    // si la casilla está ocupada te dice que cliques otra casilla
 
     if (cell.classList.contains("x") || cell.classList.contains("o")) {
         alert("Click another cell!!!");
         return;
     }
 
-    // Aconsegueix el valor de la casella clicada
     const cellId = cell.id;
 
-    // Afegeix el valor de la casella a la matriu del jugador que li toca   // Dona les classes adients a la casella clicada
     if (game.xTurn) {
         game.xState.push(cellId);
         cell.classList.add("x");
@@ -106,28 +83,25 @@ board.addEventListener('click', event => {
         cell.classList.add("o");
     }
 
-    // Comprova si hi ha guanyador
-    // aqui 
     let currentPlayer;
     if (game.xTurn === true) {
         currentPlayer = game.xState;
     } else {
         currentPlayer = game.oState;
     }
-    // aqui chequea si hay ganador y si hay pone una alerta diciendo quien ha ganado
+
     if (checkWinner(currentPlayer)) {
         if (game.xTurn === true) {
             showPopup("x");
         } else {
             showPopup("o");
         }
-        board.style.pointerEvents = "none"; // no activa el hover
+        board.style.pointerEvents = "none";
         restartContainer.classList.remove("hidden");
         turnText.classList.add("hidden");
         return;
     }
 
-    // Comprova si és empat
     const totalPlays = game.xState.length + game.oState.length;
     if (totalPlays === 9) {
         showPopup("DRAW!!!");
@@ -136,7 +110,6 @@ board.addEventListener('click', event => {
         return;
     }
 
-    // Canvia de torn
     if (game.xTurn) {
         game.xTurn = false;
     } else {
@@ -150,8 +123,6 @@ function checkWinner(playerState) {
     return game.winningStates.some(combinacion => combinacion.every(position => playerState.includes(position)));
 }
 
-
-// Botó de restart
 restartBtn.addEventListener('click', () => {
     const cell = document.querySelectorAll(".cell");
     cell.forEach(cell => cell.classList.remove("x", "o"));
@@ -165,7 +136,7 @@ restartBtn.addEventListener('click', () => {
     board.classList.remove("hidden");
     showTurn();
 
-    restartContainer.classList.add("hidden"); // Ocultar botón de nuevo
+    restartContainer.classList.add("hidden");
 });
 
 function showPopup(winner) {
